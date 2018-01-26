ff1217d502cae878aacd077dd49014de5e69aef7
Robert Wolf <wolfrob35@gmail.com>
2018-01-26 17:34:05 +0100

    fixed neltify crash

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-import github from '../img/github-icon.svg';
 import logo from '../img/logo.svg';
 import Typography from 'typography';
 import './all.sass';
