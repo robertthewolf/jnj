02a9e561f0a7ea3ed7336c45a8a5decbb506dea9
Robert Wolf <wolfrob35@gmail.com>
2018-03-11 13:23:16 +0100

    usage of gatsby templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,3 +38,42 @@ exports.onCreateNode = ({
     }
   }
 }
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  return graphql(`
+    {
+      allMarkdownRemark(filter: {frontmatter: {templateKey: { eq: "page" } }}) {
+        edges {
+          node {
+            id
+            frontmatter {
+              templateKey
+              category
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      const category = edge.node.frontmatter.category
+      createPage({
+        path: edge.node.frontmatter.category,
+        component: path.resolve(
+          `src/templates/page.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          category,
+        },
+      })
+    })
+  })
+}
--- src/pages/categories/general.md
+++ src/pages/categories/general.md
@@ -1,5 +1,5 @@
 ---
-templateKey: 'page'
+templateKey: 'setting'
 category: general
 popis: Jsme oficiální prodejce českého umění od roku 2018. Zaměřujeme se na individualitu a jedinečnost našich produktů.
 description: We are an official seller of Czech art from 2018, with a focus on individuality and uniqueness in our products.
